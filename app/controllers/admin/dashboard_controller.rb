d511557b741114cb845a7c7f1d3cfe6c69e24d1b
PaolaP <paolaperez8@gmail.com>
2022-04-16 13:20:54 -0400

    category partial and index for categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["ADMIN"], password: ENV["PASSWORD"]
+  # http_basic_authenticate_with name: ENV["ADMIN"], password: ENV["PASSWORD"]
   def show
     @product_count = Product.count
     @category_count = Category.count
