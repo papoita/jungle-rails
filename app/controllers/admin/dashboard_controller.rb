5e7746d0978c91f7b3ee15d67dcc4ff496d54bb9
PaolaP <paolaperez8@gmail.com>
2022-04-17 20:47:17 -0400

    auth works

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,11 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "jungle", password: "book"
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  # http_basic_authenticate_with name: "jungle", password: "book"
   #  http_basic_authenticate_with name: ENV["ADMIN"], password: ENV["PASSWORD"]
   def show
     @product_count = Product.count
     @category_count = Category.count
   end
+
+ 
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: "jungle", password: "book"
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  # http_basic_authenticate_with name: "jungle", password: "book"
   #  http_basic_authenticate_with name: ENV["ADMIN"], password: ENV["PASSWORD"]
   def index
     @products = Product.order(id: :desc).all
