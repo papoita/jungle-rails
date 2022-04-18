240430dbf78b48024da5037b9c00a3690286ba43
PaolaP <paolaperez8@gmail.com>
2022-04-17 20:18:15 -0400

    fix user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
+    # user.new vs user.create would save the user in db
+    user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
       redirect_to :root
