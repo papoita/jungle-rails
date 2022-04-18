940da352bd7f913978ee6d3d5de700ff266b66e9
PaolaP <paolaperez8@gmail.com>
2022-04-17 20:51:30 -0400

    delete comments

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   # http_basic_authenticate_with name: "jungle", password: "book"
-  #  http_basic_authenticate_with name: ENV["ADMIN"], password: ENV["PASSWORD"]
+ 
   def show
     @product_count = Product.count
     @category_count = Category.count
